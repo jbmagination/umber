b50139fcc501a8e89ddc7fea6e6d07be9b61dc07
Steven Penny <svnpenn@gmail.com>
2012-02-17 18:19:57 -0600

    google cache

---
+++ google.user.js
@@ -0,0 +1,7 @@
+// Get the Cached links back!
+// webcache.googleusercontent.com/search?q=cache:asdf.com
+
+var elems = document.getElementsByClassName('vshid');
+
+for (var i = 0; i < elems.length; i++)
+	elems[i].className = 0;
