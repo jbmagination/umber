b723414b6a40d37ac96301f0b8077a8a4356c346
Steven Penny <svnpenn@gmail.com>
2012-07-06 03:04:35 -0500

    M  yt.js

--- yt.js
+++ yt.js
@@ -1,56 +1,39 @@
-/* quirksmode.org/dom/innerhtml.html
- * stackoverflow.com/q/267704
- * stackoverflow.com/q/2090551
- * * * */
 function get_query_var(querystring, variable) {
-    // vars is an array of name=value pairs
-    var vars = querystring.split('&');
-    for (var i=0; i<vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (pair[0] == variable) {
-            return unescape(pair[1]);
-        }
-    }
+  // vars is an array of name=value pairs
+  var vars = querystring.split('&');
+  for (var i=0; i<vars.length; i++) {
+    var pair = vars[i].split('=');
+      if (pair[0] == variable) {
+        return unescape(pair[1]);
+      }
+  }
 }
 
 function get_quality(url) {
-    switch (true) {
-        case url.indexOf('itag=5') > 0:
-            return 'FLV H.263 240p';
-        case url.indexOf('itag=34') > 0:
-            return 'FLV H.264 Main 360p';
-        case url.indexOf('itag=35') > 0:
-            return 'FLV H.264 Main 480p';
-        case url.indexOf('itag=18') > 0:
-            return 'MP4 H.264 Baseline 360p';
-        case url.indexOf('itag=22') > 0:
-            return 'MP4 H.264 High 720p';
-        case url.indexOf('itag=37') > 0:
-            return 'MP4 H.264 High 1080p';
-        case url.indexOf('itag=43') > 0:
-            return 'WebM VP8 360p';
-        case url.indexOf('itag=44') > 0:
-            return 'WebM VP8 480p';
-        case url.indexOf('itag=45') > 0:
-            return 'WebM VP8 720p';
-        case url.indexOf('itag=46') > 0:
-            return 'WebM VP8 1080p';
-    }
+  switch (true) {
+    case url.indexOf('itag=5')   > 0: return 'FLV 240p H.263';
+    case url.indexOf('itag=34')  > 0: return 'FLV 360p H.264 Main';
+    case url.indexOf('itag=35')  > 0: return 'FLV 480p H.264 Main';
+    case url.indexOf('itag=18')  > 0: return 'MP4 360p H.264 Baseline';
+    case url.indexOf('itag=22')  > 0: return 'MP4 720p H.264 High';
+    case url.indexOf('itag=37')  > 0: return 'MP4 1080p H.264 High';
+    case url.indexOf('itag=43')  > 0: return 'WebM 360p VP8';
+    case url.indexOf('itag=44')  > 0: return 'WebM 480p VP8';
+    case url.indexOf('itag=45')  > 0: return 'WebM 720p VP8';
+    case url.indexOf('itag=102') > 0: return 'WebM 720p VP8 3D';
+    case url.indexOf('itag=46')  > 0: return 'WebM 1080p VP8';
+  }
 }
 
 var fv = document.getElementById('movie_player').getAttribute('flashvars');
-
 /* Get url_encoded_fmt_stream_map, IT IS AN ARRAY */
 var stream_map = get_query_var(fv, 'url_encoded_fmt_stream_map').split(',');
-
 var a_elements = new Array();
 
 for (var i=0; i<stream_map.length; i++) {
-
-    /* Get URL, IT IS A STRING */
-    var url = get_query_var(stream_map[i], 'url');
-
-    a_elements.push('<a href=' + url + '>' + get_quality(url) + '</a>');
+  /* Get URL, IT IS A STRING */
+  var url = get_query_var(stream_map[i], 'url');
+  a_elements.push('<a href=' + url + '>' + get_quality(url) + '</a>');
 }
 
 document.getElementById('watch-player').innerHTML = a_elements.join('<br>');
