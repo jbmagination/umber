c63c5ef5379a1549010ff059ee8e58d68ab3613f
Steven Penny <svnpenn@gmail.com>
2012-07-06 04:57:26 -0500

    M  yt.js

--- yt.js
+++ yt.js
@@ -12,19 +12,19 @@ function get_query_var(querystring, variable) {
 function get_quality(url) {
   switch (true) {
     case url.indexOf('itag=5')   > 0: return 'FLV 240p H.263';
+    case url.indexOf('itag=18')  > 0: return 'MP4 360p H.264 Baseline';
+    case url.indexOf('itag=22')  > 0: return 'MP4 720p H.264 High';
     case url.indexOf('itag=34')  > 0: return 'FLV 360p H.264 Main';
     case url.indexOf('itag=35')  > 0: return 'FLV 480p H.264 Main';
-    case url.indexOf('itag=82')  > 0: return 'MP4 360p H.264 3D';
-    case url.indexOf('itag=18')  > 0: return 'MP4 360p H.264 Baseline';
-    case url.indexOf('itag=84')  > 0: return 'MP4 720p H.264 3D';
-    case url.indexOf('itag=22')  > 0: return 'MP4 720p H.264 High';
     case url.indexOf('itag=37')  > 0: return 'MP4 1080p H.264 High';
     case url.indexOf('itag=43')  > 0: return 'WebM 360p VP8';
-    case url.indexOf('itag=100') > 0: return 'WebM 360p VP8 3D';
     case url.indexOf('itag=44')  > 0: return 'WebM 480p VP8';
     case url.indexOf('itag=45')  > 0: return 'WebM 720p VP8';
-    case url.indexOf('itag=102') > 0: return 'WebM 720p VP8 3D';
     case url.indexOf('itag=46')  > 0: return 'WebM 1080p VP8';
+    case url.indexOf('itag=82')  > 0: return 'MP4 360p H.264 3D';
+    case url.indexOf('itag=84')  > 0: return 'MP4 720p H.264 3D';
+    case url.indexOf('itag=100') > 0: return 'WebM 360p VP8 3D';
+    case url.indexOf('itag=102') > 0: return 'WebM 720p VP8 3D';
   }
 }
 
