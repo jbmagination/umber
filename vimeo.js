0d8365995defd583b0f79ac22e1a04af1ef6a462
Steven Penny <svnpenn@gmail.com>
2012-11-05 10:04:10 -0600

    D _layouts/page.md  M vimeo.js  M yt.js ?? _layouts/page.htm

--- vimeo.js
+++ vimeo.js
@@ -1,5 +1,5 @@
 // userscripts.org/scripts/review/112123
-var player = document.getElementsByClassName('player')[0].getAttribute('id');
+var player = document.getElementsByClassName("player")[0].getAttribute("id");
 player = eval(player.replace("player_", "clip"));
 var time = player.config.request.timestamp;
 var sig = player.config.request.signature;
@@ -10,11 +10,11 @@ var url = "http://player.vimeo.com/play_redirect" +
   "&sig=" + sig +
   "&time=" + time;
 
-var v = document.getElementById('video');
+var v = document.getElementById("video");
 v.style.backgroundColor = "#fff";
 v.style.fontSize = "9em";
 v.style.lineHeight = "1em";
 
 v.innerHTML =
-  '<a href="' + url + '">SD</a>, ' +
-  '<a href="' + url + '&quality=hd">HD</a>';
+  "<a href='" + url + "'>SD</a>, " +
+  "<a href='" + url + "&quality=hd'>HD</a>";
