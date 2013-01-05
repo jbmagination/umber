303f1c52411247d6f944070bc551bc50ca46afe1
Steven Penny <svnpenn@gmail.com>
2013-01-04 23:40:22 -0600

    M vimeo.js

--- vimeo.js
+++ vimeo.js
@@ -1,6 +1,6 @@
 // userscripts.org/scripts/review/112123
-var player = document.getElementsByClassName("player")[0].getAttribute("id");
-player = eval(player.replace("player_", "clip"));
+var player = document.querySelector(".player").getAttribute("id");
+player = eval (player.replace ("player_", "clip"));
 var time = player.config.request.timestamp;
 var sig = player.config.request.signature;
 var clip_id = window.location.href.substring(17);
@@ -10,10 +10,10 @@ var url = "http://player.vimeo.com/play_redirect" +
   "&sig=" + sig +
   "&time=" + time;
 
-var v = document.getElementById("menu");
-v.style.fontSize = "4em";
-v.style.lineHeight = "1em";
+var k = document.querySelector("#menu");
+k.style.fontSize = "4em";
+k.style.lineHeight = "1em";
 
-v.innerHTML =
+k.innerHTML =
   "<a href='" + url + "'>SD</a>, " +
   "<a href='" + url + "&quality=hd'>HD</a>";
