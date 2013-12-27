dee02044090748ca60e8b970712a741d3c555408
Steven Penny <svnpenn@gmail.com>
2013-12-26 21:14:40 -0600

    fix #4

--- yt.js
+++ yt.js
@@ -6,39 +6,40 @@ function get_query_var(querystring, name) {
 function get_quality(url) {
   var qual = {
     5: '240p FLV h.263',
-    17: '144p 3GP mpeg4 simple',
-    18: '360p MP4 h.264 baseline',
-    22: '720p MP4 h.264 high',
-    34: '360p FLV h.264 main',
-    35: '480p FLV h.264 main',
-    36: '240p 3GP mpeg4 simple',
-    37: '1080p MP4 h.264 high',
+    17: '144p 3GP mpeg4',
+    18: '360p MP4 h.264',
+    22: '720p MP4 h.264',
+    34: '360p FLV h.264',
+    35: '480p FLV h.264',
+    36: '240p 3GP mpeg4',
+    37: '1080p MP4 h.264',
     43: '360p WebM vp8',
     44: '480p WebM vp8',
     45: '720p WebM vp8',
     46: '1080p WebM vp8',
-    82: '360p MP4 h.264 3D',
-    84: '720p MP4 h.264 3D',
-    100: '360p WebM vp8 3D',
-    102: '720p WebM vp8 3D',
-    133: '240p MP4 DASH video',
-    134: '360p MP4 DASH video',
-    135: '480p MP4 DASH video',
-    136: '720p MP4 DASH video',
-    137: '1080p MP4 DASH video',
-    139: '48k M4A DASH audio',
-    140: '128k M4A DASH audio',
-    141: '256k M4A DASH audio',
-    160: '192p MP4 DASH video',
-    171: '128k WebM DASH audio',
-    172: '256k WebM DASH audio'
+    82: '360p MP4 3D',
+    84: '720p MP4 3D',
+    100: '360p WebM 3D',
+    102: '720p WebM 3D',
+    133: '240p MP4 DASH',
+    134: '360p MP4 DASH',
+    135: '480p MP4 DASH',
+    136: '720p MP4 DASH',
+    137: '1080p MP4 DASH',
+    139: '48k M4A DASH',
+    140: '128k M4A DASH',
+    141: '256k M4A DASH',
+    160: '192p MP4 DASH',
+    171: '128k WebM DASH',
+    172: '256k WebM DASH'
   };
   var k = url.match(/itag=(\d+)/)[1];
   return qual[k] || k;
 }
 
 var z;
-var url;
+var href;
+var qq;
 var fmts;
 var urls = [];
 var args = ytplayer.config.args;
@@ -46,19 +47,20 @@ var args = ytplayer.config.args;
 /* get "url_encoded_fmt_stream_map" querystring */
 fmts = args.url_encoded_fmt_stream_map.split(',');
 for (z = 0; z < fmts.length; z++) {
-  /* get "url" querystring */
-  url =
-    get_query_var(fmts[z], 'url') + '&signature=' +
-    get_query_var(fmts[z], 'sig');
-  urls.push('<a href=' + url + '>' + get_quality(url) + '</a>');
+  qq = get_quality(fmts[z]);
+  href =            get_query_var(fmts[z], 'url') +
+    '&signature=' + get_query_var(fmts[z], 'sig') +
+    '&title='     + args.title + ' ' + qq;
+  urls.push('<a href="' + href + '">' + qq + '</a>');
 }
 
 /* get "adaptive_fmts" querystring */
 fmts = args.adaptive_fmts ? args.adaptive_fmts.split(',') : '';
 for (z = 0; z < fmts.length; z++) {
-  /* get "url" querystring */
-  url = get_query_var(fmts[z], 'url');
-  urls.push('<a href=' + url + '>' + get_quality(url) + '</a>');
+  qq = get_quality(fmts[z]);
+  href =        get_query_var(fmts[z], 'url') +
+    '&title=' + args.title + ' ' + qq;
+  urls += '<a href="' + href + '">' + qq + '</a><br>';
 }
 
 document.body.insertAdjacentHTML(
