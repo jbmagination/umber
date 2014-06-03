11f879249f271541023baaf50417a7409f90821c
Steven Penny <svnpenn@gmail.com>
2014-06-03 17:45:51 -0500

    var src = document.querySelector('[src*=".mp4"]').getAttribute('src');

--- vimeo.js
+++ vimeo.js
@@ -1,19 +1,7 @@
-// userscripts.org/scripts/review/112123
-var player = document.querySelector(".player").getAttribute("id");
-player = eval (player.replace ("player_", "clip"));
-var time = player.config.request.timestamp;
-var sig = player.config.request.signature;
-var clip_id = window.location.href.substring(17);
-
-var url = "http://player.vimeo.com/play_redirect" +
-  "?clip_id=" + clip_id +
-  "&sig=" + sig +
-  "&time=" + time;
+var src = document.querySelector('[src*=".mp4"]').getAttribute('src');
 
 var k = document.querySelector("#menu");
 k.style.fontSize = "4em";
 k.style.lineHeight = "1em";
 
-k.innerHTML =
-  "<a href='" + url + "'>SD</a>, " +
-  "<a href='" + url + "&quality=hd'>HD</a>";
+k.innerHTML = '<a href="' + src + '">Download</a>';
