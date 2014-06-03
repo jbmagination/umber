ddef784c47d6f3c95bdfe85f87ab473c825481b0
Steven Penny <svnpenn@gmail.com>
2014-06-03 18:57:16 -0500

    var title = vimeo.config.clip.title;

--- vimeo.js
+++ vimeo.js
@@ -1,6 +1,13 @@
 var src = document.querySelector('[src*=".mp4"]').getAttribute('src');
+var title = vimeo.config.clip.title;
 
-document.querySelector('#menu').innerHTML =
-  '<a style="font-size: 2em; color: #8a0707" href="' + src + '">' +
-    vimeo.config.clip.title +
-  '</a>';
+var html = [
+  '<p>Click to copy the filename, then right click to download</p>',
+  '<a',
+  'style="font-size: 2em; color: #8a0707;"',
+  'href="' + src + '"',
+  'onclick="prompt(&quot;&quot;,&quot;' + title + '&quot;);return false"',
+  '>Link</a>'
+];
+
+document.querySelector('#menu').innerHTML = html.join(' ');
