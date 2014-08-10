a271cae1dd8a78ee14c94f32a9a1921b570c1933
Steven Penny <svnpenn@gmail.com>
2014-08-09 22:05:20 -0500

    var qy = {

--- yt.js
+++ yt.js
@@ -7,40 +7,6 @@ function qr(sr) {
   return qa;
 }
 
-function get_quality(url) {
-  var qual = {
-    5: '240p H.263 64k MP3',
-    17: '144p MPEG-4 24k AAC',
-    18: '360p H.264 96k AAC',
-    22: '720p H.264 192k AAC',
-    36: '240p MPEG-4 36k AAC',
-    43: '360p VP8 128k Vorbis',
-    82: '360p 3D 96k AAC',
-    84: '720p 3D 192k AAC',
-    100: '360p 3D 128k Vorbis',
-    133: '240p H.264',
-    134: '360p H.264',
-    135: '480p H.264',
-    136: '720p H.264',
-    137: '1080p H.264',
-    139: '48k AAC',
-    140: '128k AAC',
-    141: '256k AAC',
-    160: '144p H.264',
-    171: '128k Vorbis',
-    172: '192k Vorbis',
-    242: '240p VP9',
-    243: '360p VP9',
-    244: '480p 500k VP9',
-    245: '480p 900k VP9',
-    246: '480p 1400k VP9',
-    247: '720p VP9',
-    248: '1080p VP9'
-  };
-  var qs = qr(url);
-  return qual[qs.itag] || qs.itag
-}
-
 function rpc(tx) {
   return tx.replace('"', '&quot;', 'g');
 }
@@ -64,28 +30,54 @@ function dc(sgn) {
   return eval(sprintf('%s("%s")', fcnm, sgn));
 }
 
+var qy = {
+  _141: '256k AAC',
+   _22: '720p H.264 192k AAC',
+   _84: '720p 3D 192k AAC',
+  _140: '128k AAC',
+   _18: '360p H.264 96k AAC',
+   _82: '360p 3D 96k AAC',
+  _139: '48k AAC',
+   _36: '240p MPEG-4 36k AAC',
+   _17: '144p MPEG-4 24k AAC',
+    _5: '240p H.263 64k MP3',
+  _172: '192k Vorbis',
+  _171: '128k Vorbis',
+   _43: '360p VP8 128k Vorbis',
+  _100: '360p 3D 128k Vorbis',
+  _137: '1080p H.264',
+  _136: '720p H.264',
+  _135: '480p H.264',
+  _134: '360p H.264',
+  _133: '240p H.264',
+  _160: '144p H.264',
+  _248: '1080p VP9',
+  _247: '720p VP9',
+  _246: '480p 1400k VP9',
+  _245: '480p 900k VP9',
+  _244: '480p 500k VP9',
+  _243: '360p VP9',
+  _242: '240p VP9'
+};
+
 var args = ytplayer.config.args;
-var html = [
-  new Date().toLocaleString(),
-  'Click to copy the filename, then right click to download'
-];
 
 for (var ft of [args.url_encoded_fmt_stream_map, args.adaptive_fmts]) {
   for (var z of ft ? ft.split(',') : '') {
-    var qq = get_quality(z);
     var qs = qr(z);
-    var href = unescape(qs.url);
+    var qq = qy['_' + qs.itag] || qs.itag;
+    var hf = unescape(qs.url);
     if (qs.sig)
-      href += '&signature=' + qs.sig;
+      hf += '&signature=' + qs.sig;
     if (qs.s)
-      href += '&signature=' + dc(qs.s);
+      hf += '&signature=' + dc(qs.s);
     var fn = (args.title + '-' + qq).toLowerCase()
              .replace(/[!"&'()+.:[\]|]/g,'')
              .replace(/[ /]/g,'-')
              .replace(/-+/g,'-');
     var pm = sprintf('prompt("","%s");return false', fn)
-    html.push(
-      sprintf('<a href="%s" onclick="%s">%s</a>', href, rpc(pm), qq)
+    qy['_' + qs.itag] = sprintf(
+      '<a href="%s" onclick="%s">%s</a>', hf, rpc(pm), qq
     );
   }
 }
@@ -96,6 +88,12 @@ if (!dw) {
   dw.id = 'bm';
   document.body.insertBefore(dw, document.body.firstChild);
 }
-dw.innerHTML = html.join('<br>');
+
+dw.innerHTML = [
+  new Date().toLocaleTimeString(),
+  'Click to copy the filename, then right click to download'
+].concat(
+  keys(qy).map(ky => qy[ky]).filter(vu => /href/.test(vu))
+).join('<br>');
 
 document.querySelector('#masthead-positioner').style.position = 'static';
