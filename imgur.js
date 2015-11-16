e9a6667ea00ee58809a9df19ee5e9ee80871279b
Steven Penny <svnpenn@gmail.com>
2015-11-16 00:07:20 -0600

    hotel.style.borderRight = '10px solid green';

--- imgur.js
+++ imgur.js
@@ -1,12 +1,12 @@
 golf = document.querySelectorAll('video, img[src$="gif"]');
 for (hotel of golf) {
   if (hotel.src) {
-    hotel.style.borderRight = '1em solid green';
+    hotel.style.borderRight = '10px solid green';
     hotel.onclick = function(){this.src = this.src};
   }
   else {
     hotel.removeAttribute('loop');
-    hotel.style.borderRight = '1em solid violet';
+    hotel.style.borderRight = '10px solid violet';
     hotel.onclick = function(){this.play()};
   }
 }
