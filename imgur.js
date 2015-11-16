27eb376dbfb257c01f4b77d9a1a615896a76b179
Steven Penny <svnpenn@gmail.com>
2015-11-15 23:57:32 -0600

    if (hotel.src) {

--- imgur.js
+++ imgur.js
@@ -1,17 +1,12 @@
 golf = document.querySelectorAll('video, img[src$="gif"]');
 for (hotel of golf) {
-  if (hotel.style.display == 'none')
-    continue
-  india = document.createElement('button');
-  india.innerHTML = 'Replay';
-  india.onclick = function()  {
-    juliet = this.previousSibling;
-    if (juliet.src)
-      juliet.src = juliet.src;
-    else
-      juliet.play();
+  if (hotel.src) {
+    hotel.style.borderRight = '1em solid green';
+    hotel.onclick = function(){this.src = this.src};
   }
-  hotel.parentNode.insertBefore(india, hotel.nextSibling);
-  hotel.style.border = 0;
-  hotel.removeAttribute('loop');
+  else {
+    hotel.removeAttribute('loop');
+    hotel.style.borderRight = '1em solid violet';
+    hotel.onclick = function(){this.play()};
+  }
 }
