216cf0d77a42f9de907c9b5bbef06b4529f7de8b
Steven Penny <svnpenn@gmail.com>
2015-11-17 15:25:14 -0600

    Update imgur.js

--- imgur.js
+++ imgur.js
@@ -1,6 +1,6 @@
 golf = document.querySelectorAll('video, img[src$="gif"]');
 for (hotel of golf) {
-  hotel.className = 'after';
+  hotel.className = 'aftr';
   hotel.removeAttribute('loop');
   hotel.onclick = function() {
     if (this.src)
