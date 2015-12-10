4de3038e8c0ea3f20e368bd8c84c348307d8392d
Steven Penny <svnpenn@gmail.com>
2015-12-09 22:12:24 -0600

    addEventListener('timeupdate', function(p) p.target.loop = 0, true);

--- animation.js
+++ animation.js
@@ -1,11 +1,12 @@
-addEventListener('timeupdate', function(x) x.target.loop = 0, true)
-y = document.querySelectorAll('video[loop], img[src$=gif]');
-for (z of y) {
-  z.className = 'aftr';
-  if (z.play) {
-    z.onclick = z.onmousemove = z.onmouseenter = z.onmouseleave = 0;
-    z.onmouseover = function() this.controls = 1;
+addEventListener('timeupdate', function(p) p.target.loop = 0, true);
+q = document.querySelectorAll('video[loop], img[src$=gif]');
+for (r of q) {
+  r.className = 'aftr';
+  if (r.play) {
+    s = r.cloneNode(1);
+    s.onmouseover = function() this.controls = 1;
+    r.parentNode.replaceChild(s, r);
   }
   else
-    z.onclick = function() this.src = this.src;
+    r.onclick = function() this.src = this.src;
 }
--- animation.md
+++ animation.md
@@ -5,3 +5,7 @@ http://imgur.com/gallery/e7EEcTj/comment/530794261
 Album
 -----
 http://imgur.com/gallery/3ZlrV
+
+gfycat bullshit
+---------------
+http://gfycat.com/EcstaticWelllitHarpseal
