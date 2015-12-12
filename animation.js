1a7420967a655820ddc31932a328890a594aace8
Steven Penny <svnpenn@gmail.com>
2015-12-12 15:43:52 -0600

    addEventListener('timeupdate', function(p) p.target.loop = 0, 1);

--- animation.js
+++ animation.js
@@ -1,11 +1,11 @@
-addEventListener('timeupdate', function(p) p.target.loop = 0, true);
+addEventListener('timeupdate', function(p) p.target.loop = 0, 1);
 q = document.querySelectorAll('video[loop], img[src$=gif]');
 for (r of q) {
   r.className = 'aftr';
   if (r.play) {
-    s = r.cloneNode(1);
-    s.onmouseover = function() this.controls = 1;
-    r.parentNode.replaceChild(s, r);
+    r.onmouseover = function() this.controls = 1;
+    r.onclick = function()
+      this.parentNode.replaceChild(this.cloneNode(1), this);
   }
   else
     r.onclick = function() this.src = this.src;
--- animation.md
+++ animation.md
@@ -4,7 +4,11 @@ http://imgur.com/gallery/e7EEcTj/comment/530794261
 
 Album
 -----
-http://imgur.com/gallery/3ZlrV
+http://imgur.com/3ZlrV
+
+Long animation
+--------------
+http://imgur.com/FuCfqWZ
 
 gfycat bullshit
 ---------------
