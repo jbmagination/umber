7acd5245aa43bfc34e8c731584707522581a4b3d
Steven Penny <svnpenn@gmail.com>
2015-12-13 18:08:41 -0600

    s.firstChild.src = s.href + 'm.jpg';

--- imgur.js
+++ imgur.js
@@ -7,9 +7,9 @@ p('.left .panel').style.width = '670px';
 p('#content').style.width = '1022px';
 r = document.querySelectorAll('.cboxElement');
 for (s of r) {
-  s.firstChild.src = s.href + 'm.jpg';
   s.firstChild.style.width =
   s.firstChild.style.height =
   s.parentNode.style.width =
   s.parentNode.style.height = 'auto';
+  s.firstChild.src = s.href + 'm.jpg';
 }
