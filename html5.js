4eec4d887486249435bc3bb2a5475fc64fdb8914
Steven Penny <svnpenn@gmail.com>
2015-12-17 06:47:59 -0600

    ju = document.createElement('div');

--- html5.js
+++ html5.js
@@ -1,14 +1,14 @@
-golf = document.createElement('div');
-golf.style.background = 'white';
-document.body.insertBefore(golf, document.body.firstChild);
-hotel = document.querySelectorAll('[href*=".mp4"]');
-for (india of hotel) {
-  juliet = document.createElement('p');
-  juliet.innerHTML = india.href.replace(/.*\/|\?.*/g, '');
-  golf.appendChild(juliet);
-  kilo = document.createElement('video');
-  kilo.controls = true;
-  kilo.src = india.href;
-  kilo.style.width = '600px';
-  golf.appendChild(kilo);
+ju = document.createElement('div');
+ju.style.background = 'white';
+document.body.insertBefore(ju, document.body.firstChild);
+ki = document.querySelectorAll('[href*=.mp4]');
+for (li of ki) {
+  mi = document.createElement('p');
+  mi.innerHTML = li.href.replace(/.*\/|\?.*/g, '');
+  ju.appendChild(mi);
+  no = document.createElement('video');
+  no.controls = 1;
+  no.src = li.href;
+  no.style.width = '600px';
+  ju.appendChild(no);
 }
