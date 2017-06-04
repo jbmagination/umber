bda436f179b4f93266efc1731812f86d6a572327
Steven Penny <svnpenn@gmail.com>
2017-06-04 10:18:27 -0500

    function recurse(obc, arr, seen) {

--- libstd.js
+++ libstd.js
@@ -1,11 +1,16 @@
-function recurse(obc, arr) {
+function recurse(obc, arr, seen) {
   if (typeof arr == 'undefined')
     arr = [obc];
+  if (typeof seen == 'undefined')
+    seen = [];
+  if (seen.indexOf(obc) >= 0)
+    return;
+  seen.push(obc);
   for (ya in obc)
     if (obc.hasOwnProperty(ya)) {
       arr.push(obc[ya]);
       if (typeof obc[ya] == 'object') {
-        recurse(obc[ya], arr);
+        recurse(obc[ya], arr, seen);
       }
     }
   return arr;
