3a6ac473ae473ebcac1f0203686d484cdd578772
Steven Penny <svnpenn@gmail.com>
2017-06-04 21:56:10 -0500

    function unique(src, dep, seen) {

--- libstd.js
+++ libstd.js
@@ -1,21 +1,23 @@
-function unique(src, seen) {
+function unique(src, dep, seen) {
   if (typeof seen == 'undefined')
     seen = [];
   switch (typeof src) {
+  case 'undefined':
+  case 'symbol':
   case 'function':
-    return 'function';
+    return null;
   case 'boolean':
   case 'number':
   case 'string':
     return src;
   case 'object':
     var partial = {};
-    if (seen.indexOf(src) >= 0)
+    if (dep <= 0 || seen.indexOf(src) >= 0)
       return;
     seen.push(src);
     for (var k in src)
       if (Object.prototype.hasOwnProperty.call(src, k)) {
-        var z = unique(src[k], seen);
+        var z = unique(src[k], dep - 1, seen);
         if (z !== undefined) {
           partial[k] = z;
         }
