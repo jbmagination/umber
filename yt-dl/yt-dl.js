d0d352b65da8579df62a99def015456d0d98ce88
Steven Penny <svnpenn@gmail.com>
2017-06-25 17:24:43 -0500

    let nurl = new URL(eurl);

--- yt-dl/yt-dl.js
+++ yt-dl/yt-dl.js
@@ -71,14 +71,16 @@ if (ya.length) {
 gvd = null;
 
 for (let eurl of durl) {
-  let usp = new URLSearchParams(eurl);
-  let efmt = cfmt['_' + usp.get('itag')] || usp.get('itag');
+  let nurl = new URL(eurl);
+  let nusp = nurl.searchParams;
+  nusp.set('ratebypass', 'yes');
+  let efmt = cfmt['_' + nusp.get('itag')] || nusp.get('itag');
   let fnam = (ytplayer.config.args.title + ' ' + efmt).replace(' AAC', '')
     .replace(' H.264', '').replace(/[!"#&'()*,:?@|~’”]/g, '')
     .replace(/[+./[\]]/g, ' ').replace(/ +/g, ' ').toLowerCase();
   let opro = `prompt("", "${fnam}"); return false`.replace(/"/g, '&quot;');
-  cfmt['_' + usp.get('itag')] =
-  `<a href="${eurl}&ratebypass" onclick="${opro}">${efmt}</a>`;
+  cfmt['_' + nusp.get('itag')] =
+  `<a href="${nurl.href}" onclick="${opro}">${efmt}</a>`;
 }
 
 let fdiv = document.querySelector('#bm');
