731ff3717b88eca590b79aead1cdaf24b15d2343
Steven Penny <svnpenn@gmail.com>
2018-01-12 20:18:45 -0600

    bmklet/html5.js: add 1080p and 720p

--- bmklet/html5.js
+++ bmklet/html5.js
@@ -1,14 +1,13 @@
-ju = document.createElement('div');
-ju.style.background = 'white';
-document.body.insertBefore(ju, document.body.firstChild);
-ki = document.querySelectorAll('[href*="mp4"]');
-for (li of ki) {
-  mi = document.createElement('p');
-  mi.innerHTML = li.href.replace(/.*\/|\?.*/g, '');
-  ju.appendChild(mi);
-  no = document.createElement('video');
-  no.controls = 1;
-  no.src = li.href;
-  no.style.width = '600px';
-  ju.appendChild(no);
+br = document.createElement('div');
+br.style.background = 'white';
+document.body.prepend(br);
+pa = document.querySelectorAll('[href*="1080p"],[href*="720p"],[href*="mp4"]');
+for (xr of pa) {
+  ya = document.createElement('p');
+  ya.innerHTML = xr.href;
+  zu = document.createElement('video');
+  zu.controls = 1;
+  zu.src = xr.href;
+  zu.style.width = '600px';
+  br.append(ya, zu);
 }
