3c718477e3f2e65fd4f529222c6b8f97b52bc164
Steven Penny <svnpenn@gmail.com>
2018-01-12 21:36:40 -0600

    zu.controls = zu.style.width = '600px';

--- bmklet/html5.js
+++ bmklet/html5.js
@@ -5,8 +5,7 @@ document.querySelectorAll('[href*="1080p"],[href*="720p"],[href*="mp4"]')
 .forEach(xr => {
   ya = document.createElement('p');
   zu = document.createElement('video');
-  zu.controls = 1;
-  zu.style.width = '600px';
+  zu.controls = zu.style.width = '600px';
   ya.textContent = zu.src = xr.href;
   br.append(ya, zu);
 });
