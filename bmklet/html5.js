2df98334ba66ec7570994ca4700530269b93f6cc
Steven Penny <svnpenn@gmail.com>
2018-01-12 21:28:29 -0600

    ya.textContent = zu.src = xr.href;

--- bmklet/html5.js
+++ bmklet/html5.js
@@ -4,10 +4,9 @@ document.body.prepend(br);
 document.querySelectorAll('[href*="1080p"],[href*="720p"],[href*="mp4"]')
 .forEach(xr => {
   ya = document.createElement('p');
-  ya.textContent = xr.href;
   zu = document.createElement('video');
   zu.controls = 1;
-  zu.src = xr.href;
   zu.style.width = '600px';
+  ya.textContent = zu.src = xr.href;
   br.append(ya, zu);
 });
