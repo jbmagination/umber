99e37030790be477437e8dbeb94a542404536741
Steven Penny <svnpenn@gmail.com>
2018-01-12 20:56:55 -0600

    bmklet/html5.js: forEach

--- bmklet/html5.js
+++ bmklet/html5.js
@@ -1,8 +1,8 @@
 br = document.createElement('div');
 br.style.background = 'white';
 document.body.prepend(br);
-pa = document.querySelectorAll('[href*="1080p"],[href*="720p"],[href*="mp4"]');
-for (xr of pa) {
+document.querySelectorAll('[href*="1080p"],[href*="720p"],[href*="mp4"]')
+.forEach(xr => {
   ya = document.createElement('p');
   ya.innerHTML = xr.href;
   zu = document.createElement('video');
@@ -10,4 +10,4 @@ for (xr of pa) {
   zu.src = xr.href;
   zu.style.width = '600px';
   br.append(ya, zu);
-}
+});
