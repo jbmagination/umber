da68b50fa0dd2fa5c7fa4e3267799266f5ba203c
Steven Penny <svnpenn@gmail.com>
2018-01-23 06:51:35 -0600

    dc = document;

--- bmklet/html5.js
+++ bmklet/html5.js
@@ -1,10 +1,11 @@
-br = document.createElement('div');
+dc = document;
+br = dc.createElement('div');
 br.style.background = 'white';
-document.body.prepend(br);
-document.querySelectorAll('[href*="1080p"],[href*="720p"],[href*="mp4"]')
-.forEach(xr => {
-  ya = document.createElement('p');
-  zu = document.createElement('video');
+dc.body.prepend(br);
+dc.querySelectorAll(`[href*="1080p"],[href*="720p"],[href*="400p"],
+[href*="mp4"]`).forEach(xr => {
+  ya = dc.createElement('p');
+  zu = dc.createElement('video');
   zu.controls = zu.style.width = '600px';
   ya.textContent = zu.src = xr.href;
   br.append(ya, zu);
