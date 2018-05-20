51edbe2669d2ad13745c9d311ad31fe88f68c5e9
Steven Penny <svnpenn@gmail.com>
2018-05-19 20:52:14 -0500

    modified:   bmklet/multimedia/video.js

--- bmklet/multimedia/video.js
+++ bmklet/multimedia/video.js
@@ -1,12 +1,13 @@
 rm = document.querySelectorAll('video');
-qs = document.querySelectorAll(
+
+new Set([...document.querySelectorAll(
   '[href*="1080p"],[href*="720p"],[href*="400p"],[href*="mp4"],[src*="mp4"]'
-);
-new Set([...qs].map(fe => fe.href || fe.src)).forEach(fe => {
+)].map(fe => fe.href || fe.src)).forEach(fe => {
   pm = document.createElement('p');
   vd = document.createElement('video');
   vd.controls = vd.style.width = '600px';
   pm.textContent = vd.src = fe;
   document.body.prepend(pm, vd);
 });
+
 rm.forEach(fe => fe.remove());
