885dbc9cfc001cc68a02ef232d80f50be28b4418
Steven Penny <svnpenn@gmail.com>
2018-09-09 12:19:16 -0500

    document.querySelectorAll('.comments, .deepthread a').forEach

--- bmklet/reddit/depth.js
+++ bmklet/reddit/depth.js
@@ -1,9 +1,9 @@
 /* cloneNode retains event for some reason */
 'use strict';
-document.querySelectorAll('.comments').forEach(xr => {
+document.querySelectorAll('.comments, .deepthread a').forEach(xr => {
    var zu = document.createElement('a');
    zu.style.border = 'thick solid green';
    zu.textContent = xr.textContent;
-   zu.href = xr.href + '?depth=2';
+   zu.href = xr.href + '?depth=3';
    xr.replaceWith(zu);
 });
