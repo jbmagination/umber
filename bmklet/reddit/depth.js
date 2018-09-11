fdcdb37507c8657d062c0053e3ef85d9eff9d72f
Steven Penny <svnpenn@gmail.com>
2018-09-10 19:11:13 -0500

    zu.style.border = 'medium solid';

--- bmklet/reddit/depth.js
+++ bmklet/reddit/depth.js
@@ -2,7 +2,7 @@
 'use strict';
 document.querySelectorAll('.comments, .deepthread a').forEach(xr => {
    var zu = document.createElement('a');
-   zu.style.border = 'thick solid green';
+   zu.style.border = 'medium solid';
    zu.textContent = xr.textContent;
    zu.href = xr.href + '?depth=3';
    xr.replaceWith(zu);
