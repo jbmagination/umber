4d041e88f3829fec916890194e92d40339a8dd96
Steven Penny <svnpenn@gmail.com>
2018-09-14 22:39:11 -0500

    deleted:    reddit/decades.js

--- bmklet/reddit/decades.js
+++
@@ -1,6 +0,0 @@
-alert(
-  [...new Set(
-    [...document.querySelectorAll('.linkflairlabel')]
-    .map(z => z.textContent.substr(0, 3))
-  )].sort().reverse()
-);
--- bmklet/reddit/dim.js
+++ bmklet/reddit/dim.js
@@ -1,6 +1,8 @@
 q = document.querySelectorAll('.comment .entry');
-for (z of q) {
-  if (z.textContent.length < 300) {
-    z.style.opacity = .3;
-  }
+for (z of q)
+{
+   if (z.textContent.length < 300)
+   {
+      z.style.opacity = .3;
+   }
 }
