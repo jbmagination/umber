6ad35efe0baf1afde7ba04e3ac2855aed62f05d1
Steven Penny <svnpenn@gmail.com>
2018-09-14 22:48:52 -0500

    new Set

--- bmklet/multimedia/video.js
+++ bmklet/multimedia/video.js
@@ -1,13 +1,20 @@
 rm = document.querySelectorAll('video');
-
-new Set([...document.querySelectorAll(
-  '[href*="1080p"],[href*="720p"],[href*="400p"],[href*="mp4"],[src*="mp4"]'
-)].map(fe => fe.href || fe.src)).forEach(fe => {
-  pm = document.createElement('p');
-  vd = document.createElement('video');
-  vd.controls = vd.style.width = '600px';
-  pm.textContent = vd.src = fe;
-  document.body.prepend(pm, vd);
+new Set
+([
+   ...document.querySelectorAll
+   ([
+      '[href*="1080p"]',
+      '[href*="400p"]',
+      '[href*="720p"]',
+      '[href*="mp4"]',
+      '[src*="mp4"]'
+   ].join())
+].map(fe => fe.href || fe.src)).forEach
+(fe => {
+   pm = document.createElement('p');
+   vd = document.createElement('video');
+   vd.controls = vd.style.width = '600px';
+   pm.textContent = vd.src = fe;
+   document.body.prepend(pm, vd);
 });
-
 rm.forEach(fe => fe.remove());
--- bmklet/reddit/data-url.js
+++ bmklet/reddit/data-url.js
@@ -1,12 +1,10 @@
 'use strict';
 document.querySelectorAll('.link.thing').forEach(wh => {
-  var
-  xr = wh.querySelector('a.title'),
-  /* cloneNode retains event for some reason */
-  zu = document.createElement('a');
-  zu.style.border = 'thick solid green';
-  zu.className = 'title';
-  zu.textContent = xr.textContent;
-  zu.href = wh.dataset.url;
-  xr.replaceWith(zu);
+   /* cloneNode retains event for some reason */
+   var xr = wh.querySelector('a.title'), zu = document.createElement('a');
+   zu.style.border = 'thick solid green';
+   zu.className = 'title';
+   zu.textContent = xr.textContent;
+   zu.href = wh.dataset.url;
+   xr.replaceWith(zu);
 });
