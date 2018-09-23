ea07442ec0505363deb5e06696f40f82fedf0e95
Steven Penny <svnpenn@gmail.com>
2018-09-23 16:42:20 -0500

    new Set([...document.querySelectorAll([

--- bmklet/multimedia/video.js
+++ bmklet/multimedia/video.js
@@ -1,16 +1,11 @@
 rm = document.querySelectorAll('video');
-new Set
-([
-   ...document.querySelectorAll
-   ([
-      '[href*="1080p"]',
-      '[href*="400p"]',
-      '[href*="720p"]',
-      '[href*="mp4"]',
-      '[src*="mp4"]'
-   ].join())
-].map(fe => fe.href || fe.src)).forEach
-(fe => {
+new Set([...document.querySelectorAll([
+   '[href*="1080p"]',
+   '[href*="400p"]',
+   '[href*="720p"]',
+   '[href*="mp4"]',
+   '[src*="mp4"]'
+].join())].map(fe => fe.href || fe.src)).forEach(fe => {
    pm = document.createElement('p');
    vd = document.createElement('video');
    vd.controls = vd.style.width = '600px';
--- bmklet/youtube/download.js
+++ bmklet/youtube/download.js
@@ -1,7 +1,7 @@
 'use strict';
 function flatten(src, path = [], seen = new Map())
 {
-   for (let [ky, vu] of Object.entries(src))
+   for (var [ky, vu] of Object.entries(src))
    {
       if (typeof vu == 'object' && vu != null)
       {
@@ -14,8 +14,8 @@ function flatten(src, path = [], seen = new Map())
    }
    if (!path.length)
    {
-      let op = {};
-      for (let [oc, pt] of seen)
+      var op = {};
+      for (var [oc, pt] of seen)
       {
          Object.keys(oc).filter(x => typeof oc[x] == 'string')
          .forEach(x => op[[...pt, x]] = oc[x]);
@@ -23,55 +23,52 @@ function flatten(src, path = [], seen = new Map())
       return op;
    }
 }
-let ypsi = flatten(ytPubsubPubsubInstance);
+var ypsi = flatten(ytPubsubPubsubInstance);
 
-let durl = Object.values(ypsi).filter
-(
+var durl = Object.values(ypsi).filter(
    x => x.includes('videoplayback?') && !x.includes('range=')
 );
 
-let dsig = new Set(Object.keys(ypsi).filter
-(
+var dsig = new Set(Object.keys(ypsi).filter(
    x => x.includes(',signature')
 ).map(x => ypsi[x])).values();
 
-let vqua = Object.keys(ypsi).filter
-(
+var vqua = Object.keys(ypsi).filter(
    x => x.includes(',qualityLabel')
 ).map(x => ypsi[x])[Symbol.iterator]();
 
 ypsi = [];
-for (let eurl of durl)
+for (var eurl of durl)
 {
-   let nurl = new URL(eurl);
-   let nusp = nurl.searchParams;
+   var nurl = new URL(eurl);
+   var nusp = nurl.searchParams;
    nusp.set('ratebypass', 'yes');
    if (!nusp.get('signature'))
    {
       nusp.set('signature', dsig.next().value);
    }
-   let squa = nusp.get('mime').includes('video') ?
+   var squa = nusp.get('mime').includes('video') ?
       vqua.next().value :
       ~~(nusp.get('clen') * 8 / (1000 * nusp.get('dur'))) + 'k';
-   ypsi.push
-   (
+   ypsi.push(
       `<!--${nusp.get('mime') + `${9999 - parseInt(squa)}`.padStart(4)}-->
       <p><a href="${nurl.href}">${squa} ${nusp.get('mime')}</a></p>`
    );
 }
-let fdiv = document.querySelector('#yt-dl');
+var fdiv = document.querySelector('#yt-dl');
 if (!fdiv)
 {
    fdiv = document.createElement('div');
    fdiv.id = 'yt-dl';
    fdiv.style.background = 'plum';
-   fdiv.style.border-radius = '1em';
+   fdiv.style.borderRadius = '2ex';
    fdiv.style.bottom = 0;
-   fdiv.style.padding = '1em';
+   fdiv.style.padding = '2ex';
    fdiv.style.position = 'fixed';
    fdiv.style.right = 0;
-   fdiv.style.width = '12em';
-   fdiv.style.z-index = 6;
+   fdiv.style.width = '25ex';
+   fdiv.style.zIndex = 6;
+
    document.body.prepend(fdiv);
 }
 fdiv.innerHTML = ypsi.sort().join('');
