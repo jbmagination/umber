035db0cf29dcd4683f60419c0fdccbb09b1c4e4d
Steven Penny <svnpenn@gmail.com>
2018-10-20 18:08:18 -0500

    new file:   bmklet/alternativeto/disco.js

---
+++ bmklet/alternativeto/disco.js
@@ -0,0 +1,8 @@
+'use strict';
+document.querySelectorAll('.custom-icon-discontinued').forEach(z => {
+   while (!z.dataset.mainitemid)
+   {
+      z = z.parentNode;
+   }
+   z.style.display = 'none';
+});
