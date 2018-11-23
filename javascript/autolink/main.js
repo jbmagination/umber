051b5d7b9705f1be5bbeb414e98924fc2fccc292
Steven Penny <svnpenn@gmail.com>
2018-11-23 14:33:29 -0600

    RegExp('https?://[^ "<]+(?![^<>]*(>|</a>))', 'g'), '<a

--- javascript/autolink/main.js
+++ javascript/autolink/main.js
@@ -1,7 +1,6 @@
 'use strict';
 document.querySelectorAll('p, li').forEach(z =>
    z.innerHTML = z.innerHTML.replace(
-      RegExp('(?!href=")https?://[^ "<]+(?![^<]*</a>)', 'g'),
-      '<a href="$&">$&</a>'
+      RegExp('https?://[^ "<]+(?![^<>]*(>|</a>))', 'g'), '<a href="$&">$&</a>'
    )
 );
