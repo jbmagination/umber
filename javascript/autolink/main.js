e0dc8bf62a22d7eb25bd7b1a3cc2f42548600279
Steven Penny <svnpenn@gmail.com>
2018-11-23 13:32:24 -0600

    new file:   javascript/autolink/test.html

--- javascript/autolink/main.js
+++ javascript/autolink/main.js
@@ -1,10 +1,7 @@
 'use strict';
-document.querySelectorAll('p, li').forEach(b => {
-   let k = b.innerHTML;
-   if (RegExp('^https?://').test(k))
-   {
-      let z = document.createElement('a');
-      z.href = z.textContent = k;
-      b.firstChild.replaceWith(z);
-   }
-});
+document.querySelectorAll('p, li').forEach(z =>
+   z.innerHTML = z.innerHTML.replace(
+      RegExp('(?!href=")https?://[^ "<]+(?![^<]*</a>)', 'g'),
+      '<a href="$&">$&</a>'
+   )
+);
--- javascript/autolink/readme.md
+++ javascript/autolink/readme.md
@@ -1,31 +1,6 @@
 Autolink
 ========
 
-Yes
------
-
-~~~html
-<p>http://example.com</p>
-~~~
-
-~~~html
-<li>http://example.com</li>
-~~~
-
-No
------
-
-~~~html
-<code>http://example.com</code>
-~~~
-
-Result
-------
-
-~~~html
-<p><a href="http://example.com">http://example.com</a></p>
-~~~
-
 References
 ----------
 
