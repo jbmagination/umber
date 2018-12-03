7421a69d17ef047853d82e0da5ff09030dd45ffc
Steven Penny <svnpenn@gmail.com>
2018-12-03 08:01:52 -0600

    #!/bin/dash -e

--- shell/exec-repl.sh
+++ shell/exec-repl.sh
@@ -1,7 +1,23 @@
-#!/bin/dash
-exec php -r '
-while ($_q = fgets(STDIN))
-{
-   print "=> " . var_export(eval("return " . $_q), 1) . "\n";
-}
-' "$@"
+#!/bin/dash -e
+case $1 in
+perl)
+   shift
+   perl -n -E 'say "=> ", eval' - "$@"
+   ;;
+php)
+   shift
+   exec php -r '
+   while ($_q = fgets(STDIN))
+   {
+      print "=> " . var_export(eval("return " . $_q), 1) . "\n";
+   }
+   ' "$@"
+   ;;
+*)
+   echo 'synopsis: exec-repl.sh <language> [arguments]
+
+language:
+- perl
+- php'
+   exit 1
+esac
