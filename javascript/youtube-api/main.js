3ab79315995f7eb8a9d981fe1dddee89c451c2d7
Steven Penny <svnpenn@gmail.com>
2018-12-25 09:59:16 -0600

    new file:   javascript/youtube-api/style.css

--- javascript/youtube-api/main.js
+++ javascript/youtube-api/main.js
@@ -15,9 +15,20 @@ async function main()
    });
 
    (await (await fetch(ce)).json()).items.forEach(dh => {
-      let ab = document.createElement('img');
-      ab.src = dh.snippet.thumbnails.medium.url;
-      document.getElementById('figures').append(ab);
+      let e_fu = document.createElement('figure');
+      let e_a = document.createElement('a');
+      let e_i = document.createElement('img');
+      let e_fc = document.createElement('figcaption');
+      let e_p1 = document.createElement('p');
+      let e_p2 = document.createElement('p');
+      e_a.href = 'https://www.youtube.com/watch?v=' + dh.id.videoId;
+      e_i.src = dh.snippet.thumbnails.medium.url;
+      e_p1.textContent = dh.snippet.title;
+      e_p2.textContent = dh.snippet.publishedAt;
+      e_a.append(e_i);
+      e_fc.append(e_p1, e_p2);
+      e_fu.append(e_a, e_fc);
+      document.getElementById('figures').append(e_fu);
    });
 }
 main();
