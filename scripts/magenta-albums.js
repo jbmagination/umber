f1671719604f8dd8840cd825f4e1a9c4cfda6877
Steven Penny <svnpenn@gmail.com>
2018-12-30 20:23:24 -0600

    Hardwired… to Self‐Destruct (2016):

--- scripts/magenta-albums.js
+++ scripts/magenta-albums.js
@@ -1,14 +1,15 @@
 'use strict';
-var ab = (ce, dh = document, fk) => dh[`querySelector${fk ? 'All' : ''}`](ce);
+var ab = (ce, fk = '', dh = document) => dh['querySelector' + fk](ce);
 var np = [];
 
-ab('tr', ab('tbody'), 1).forEach(ce => {
-   var dh = ab('td', ce, 1);
+ab('tr', 'All', ab('tbody')).forEach(ce => {
+   var dh = ab('td', 'All', ce);
    np.unshift(`   ${dh[1].textContent} (${dh[0].textContent}):`);
 });
 
 var oq = ab('textarea') || document.createElement('textarea');
-oq.cols = oq.rows = 30;
+oq.cols = 40;
+oq.rows = 20;
 oq.style.font = '2.5ex Consolas';
 oq.textContent = np.join('\n');
 ab('#sidebar').prepend(oq);
