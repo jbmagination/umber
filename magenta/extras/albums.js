7e7af66f8f09b6735acc8f00f8434219a2a8f170
Steven Penny <svnpenn@gmail.com>
2019-02-02 09:42:28 -0600

    var $ = (ae, bd = document) => bd.querySelector(ae);

--- magenta/extras/albums.js
+++ magenta/extras/albums.js
@@ -1,15 +1,19 @@
 'use strict';
-var ab = (ce, fk = '', dh = document) => dh['querySelector' + fk](ce);
-var np = [];
+var $ = (ae, bd = document) => bd.querySelector(ae);
+var $$ = (ae, bd = document) => bd.querySelectorAll(ae);
+var cf = [];
 
-ab('tr', 'All', ab('tbody')).forEach(ce => {
-   var dh = ab('td', 'All', ce);
-   np.unshift(`   ${dh[1].textContent} (${dh[0].textContent}):`);
+$$('tr', $('tbody')).forEach(bd => {
+   var hk = $$('td', bd);
+   cf.unshift(
+      '   ' + hk[1].textContent + ':',
+      '      <year: ' + hk[0].textContent
+   );
 });
 
-var oq = ab('textarea') || document.createElement('textarea');
-oq.cols = 40;
-oq.rows = 20;
-oq.style.font = '2.5ex Consolas';
-oq.textContent = np.join('\n');
-ab('#sidebar').prepend(oq);
+var np = $('textarea') || document.createElement('textarea');
+np.cols = 40;
+np.rows = 20;
+np.style.font = '2.5ex Consolas';
+np.textContent = cf.join('\n');
+$('#sidebar').prepend(np);
