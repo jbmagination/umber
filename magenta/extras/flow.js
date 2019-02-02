c6afafbe6942b5a8863a157e1c9f4e82c78003b2
Steven Penny <svnpenn@gmail.com>
2019-02-02 14:43:02 -0600

    var $ = ae => document.querySelector(ae);

--- magenta/extras/flow.js
+++ magenta/extras/flow.js
@@ -1,80 +1,75 @@
 'use strict';
-var qs = sc => document.querySelector(sc);
+var $ = ae => document.querySelector(ae);
+var $$ = ae => document.querySelectorAll(ae);
+
+/* we need to base this number on the watch page and not the result page for
+accuracy. this will give us some false highs but that is better than false
+lows. */
+var thold = 6000000;
+
 /* get publish date */
-var dp = qs('[itemprop=datePublished]');
+var dp = $('[itemprop=datePublished]');
+
 if (dp)
 {
    /* get views */
-   var vw = qs('[itemprop=interactionCount]').content;
+   var vw = $('[itemprop=interactionCount]').content;
+
    /* get years */
    var yr = (Date.now() - Date.parse(dp.content)) / Date.parse(1971);
-   /* prepend */
-   if (!qs('textarea'))
-   {
-      var tx = document.createElement('textarea');
-      tx.cols = 50;
-      tx.style.font = '2ex Consolas';
-      tx.textContent = '      $: ' + Math.floor(vw / yr).toLocaleString() +
-         ' youtu.be/' + qs('[itemprop=videoId]').content;
-      qs('#watch7-sidebar').prepend(tx);
-   }
+
+   var xc = document.createElement('textarea');
+   xc.cols = 50;
+   xc.style.bottom = xc.style.right = 0;
+   xc.style.font = 'medium Consolas';
+   xc.style.position = 'fixed';
+   xc.style.zIndex = 6;
+   xc.textContent = [
+      '<flow: ' + Math.floor(vw / yr).toLocaleString(),
+      '<link: https://youtu.be/' + $('[itemprop=videoId]').content
+   ].map(ae => '      ' + ae).join('\n');
+   document.body.append(xc);
 }
 else
 {
-   document.querySelectorAll('.yt-lockup-meta-info').forEach(ul => {
+   $$('.yt-lockup-meta-info').forEach(ul => {
       if (!ul.children[1])
       {
          return;
       }
-      var t_n, t_w, ratio, views;
+
       /* 1 year ago, Streamed 1 year ago */
-      [t_n, t_w] = ul.children[0].textContent.split(' ').slice(-3);
+      var [t_n, t_w] = ul.children[0].textContent.split(' ').slice(-3);
+
       /* get views */
-      views = ul.children[1].textContent.split(' ')[0].replace(/,/g, '');
-      /* get ratio */
-      switch (t_w)
-      {
+      var views = ul.children[1].textContent.split(' ')[0].replace(/,/g, '');
+
+      switch (t_w) {
       case 'year':
       case 'years':
-         ratio = views / t_n;
+         var fw = views / t_n;
          break;
       case 'month':
       case 'months':
-         ratio = views * 12 / t_n;
+         var fw = views * 12 / t_n;
          break;
       case 'week':
       case 'weeks':
-         ratio = views * 52 / t_n;
+         var fw = views * 52 / t_n;
          break;
       case 'day':
       case 'days':
-         ratio = views * 365.25 / t_n;
+         var fw = views * 365.25 / t_n;
          break;
       case 'hour':
       case 'hours':
-         ratio = views * 24 * 365.25 / t_n;
+         var fw = views * 24 * 365.25 / t_n;
          break;
       case 'minute':
       case 'minutes':
-         ratio = views * 60 * 24 * 365.25 / t_n;
-         break;
-      default:
-         ratio = 'bad';
+         var fw = views * 60 * 24 * 365.25 / t_n;
       }
-      if (ratio == 'bad')
-      {
-         ul.style.border = 'thick solid yellow';
-      }
-      /* we need to base this number on the watch page and not the result page
-      for accuracy. this will give us some false highs but that is better than
-      false lows. */
-      else if (ratio < 6000000)
-      {
-         ul.style.border = 'thick solid green';
-      }
-      else
-      {
-         ul.style.border = 'thick solid red';
-      }
+
+      ul.style.border = fw < thold ? 'thick solid green' : 'thick solid red';
    });
 }
