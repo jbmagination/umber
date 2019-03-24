e8cfcaf2e2d1a6c4fdc1efdf0620e5fa461b4fa3
Steven Penny <svnpenn@gmail.com>
2019-03-23 20:59:37 -0500

    let spar = new URLSearchParams(location.search);

--- video/assets/main.js
+++ video/assets/main.js
@@ -2,37 +2,58 @@
 
 async function main()
 {
-   let ab = new URLSearchParams(location.search);
-   let ce = new URL('https://www.googleapis.com/youtube/v3/search');
-   ce.searchParams.set('key', 'AIzaSyCrNB6t8QVxyjXpTSXwpWGCu-kR35Ba8JQ');
-   ce.searchParams.set('maxResults', 50);
-   ce.searchParams.set('order', 'date');
-   ce.searchParams.set('part', 'snippet');
+   let spar = new URLSearchParams(location.search);
+   let furl = new URL('https://www.googleapis.com/youtube/v3/playlistItems');
+   furl.searchParams.set('key', 'AIzaSyCrNB6t8QVxyjXpTSXwpWGCu-kR35Ba8JQ');
+   furl.searchParams.set('maxResults', 50);
+   furl.searchParams.set('part', 'snippet');
+   furl.searchParams.set('playlistId', spar.get('playlistId'));
+   document.getElementById('playlistId').value = spar.get('playlistId');
 
-   ['channelId', 'publishedBefore'].forEach(dh => {
-      if (ab.get(dh)) {
-         ce.searchParams.set(dh, ab.get(dh));
-         document.getElementById(dh).value = ab.get(dh);
-      }
-   });
+   if (spar.get('pageToken'))
+   {
+      furl.searchParams.set('pageToken', spar.get('pageToken'));
+   }
 
-   ab = await (await fetch(ce)).json();
-   document.title = ab.items[0].snippet.channelTitle + ' - Umber Video';
-   ab.items.forEach(dh => {
+   let pitm = await (await fetch(furl)).json();
+   document.title = pitm.items[0].snippet.channelTitle + ' - Umber Video';
+   pitm.items.forEach(ab => {
       let e_fu = document.createElement('figure');
       let e_a = document.createElement('a');
       let e_i = document.createElement('img');
       let e_d = document.createElement('div');
       let e_fc = document.createElement('figcaption');
-      e_a.href = 'https://www.youtube.com/watch?v=' + dh.id.videoId;
-      e_i.src = dh.snippet.thumbnails.medium.url;
+      e_a.href = 'https://www.youtube.com/watch?v=' +
+         ab.snippet.resourceId.videoId;
+      e_i.src = ab.snippet.thumbnails.medium.url;
       // needs to cover &#39;
-      e_d.innerHTML = dh.snippet.title;
-      e_fc.textContent = dh.snippet.publishedAt;
+      e_d.innerHTML = ab.snippet.title;
+      e_fc.textContent = ab.snippet.publishedAt;
       e_a.append(e_i, e_d);
       e_fu.append(e_a, e_fc);
       document.getElementById('figures').append(e_fu);
    });
+
+   if (pitm.nextPageToken)
+   {
+      spar.set('pageToken', pitm.nextPageToken);
+      document.getElementById('next').href = '?' + spar.toString();
+   }
+   else
+   {
+      document.getElementById('next').remove();
+   }
+
+   if (pitm.prevPageToken)
+   {
+      spar.set('pageToken', pitm.prevPageToken);
+      document.getElementById('prev').href = '?' + spar.toString();
+   }
+   else
+   {
+      document.getElementById('prev').remove();
+   }
+
 }
 
 main();
--- video/docs/readme.md
+++ video/docs/readme.md
@@ -1,25 +1,28 @@
 Video docs
 ==========
 
-When you delete a project, it disables the API and deletes the credentials.
-
-YouTube API Explorer
---------------------
-
-- https://developers.google.com/apis-explorer#p/youtube/v3/youtube.search.list
-- https://developers.google.com/youtube/v3/docs/search/list
-
-Enable API
-----------
-
-https://console.developers.google.com/apis/library/youtube.googleapis.com
-
-Create project
---------------
+To use this site you need a YouTube API key. First, visit this page:
 
 https://console.developers.google.com/cloud-resource-manager
 
-Create credentials
-------------------
+1. Click CREATE PROJECT
+2. Project name: YouTube
+3. Click CREATE
+
+then you need to enable the YouTube API. Visit this page:
+
+https://console.developers.google.com/apis/library/youtube.googleapis.com
+
+and click ENABLE. Finally, you need to create credentials. Visit this page:
 
 https://console.developers.google.com/apis/credentials
+
+1. Click Create credentials
+2. Click API key
+
+If you wish to undo all of this, visit the first link on this page and delete
+the project. It will automatically disable the API and delete the credentials.
+The docs for the API used for this project are here:
+
+- https://developers.google.com/apis-explorer#p/youtube/v3/youtube.playlistItems.list
+- https://developers.google.com/youtube/v3/docs/playlistItems/list
