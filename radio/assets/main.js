a2482a570e32a91623d92ea8f47a31136a2ffce0
Steven Penny <svnpenn@gmail.com>
2019-06-04 23:00:06 -0500

    const slug = function(txt) {

--- radio/assets/main.js
+++ radio/assets/main.js
@@ -1,9 +1,8 @@
 'use strict';
-
 const gei = bb => document.getElementById(bb);
 
 // slug is not required, but it will allow for history search
-const slug = txt => {
+const slug = function(txt) {
    return txt.replace(/ ?[&,-] /g, '-')
    .replace(/[ /]/g, '-')
    .replace(/[%().’]/g, '')
@@ -11,80 +10,92 @@ const slug = txt => {
    .toLowerCase();
 };
 
-const fgr = vdeo => {
+const bandcamp = function(rec) {
+   // case sensitive
+   const aurl = new URL('https://bandcamp.com');
+   aurl.hash = slug(rec[3]);
+   aurl.searchParams.set('track', rec[2].split('/')[0]);
+   // required when protocol is not "file:"
+   aurl.searchParams.set('ref', '');
+   // these are not required, but they look nicer
+   aurl.searchParams.set('artwork', 'small');
+   aurl.searchParams.set('size', 'large');
+   const iurl = new URL('https://f4.bcbits.com');
+   iurl.pathname = 'img/' + rec[2].split('/')[1] + '_16.jpg';
+   return [aurl, iurl];
+};
+
+const fgr = function() {
+   const tstamp = arguments[0];
+   const year = arguments[1];
+   const [site, url1, url2] = arguments[2].split('/');
+   const title = arguments[3];
    const e_fu = document.createElement('figure');
    const e_a = document.createElement('a');
    const e_d1 = document.createElement('div');
    const e_i = document.createElement('img');
    const e_d2 = document.createElement('div');
    const e_fc = document.createElement('figcaption');
-   const attr = vdeo[2].split('/');
 
-   // need this else we get SyntaxError: redeclaration of let url
-   let url;
-   switch (attr.shift()) {
+   // need this else we get SyntaxError: redeclaration of let link
+   let link;
+   switch (site) {
    case 'b':
       // case sensitive
-      url = new URL('https://bandcamp.com/EmbeddedPlayer');
-      url.hash = slug(vdeo[3]);
-      url.searchParams.set('track', attr.shift());
+      link = new URL('https://bandcamp.com/EmbeddedPlayer');
+      link.hash = slug(title);
+      link.searchParams.set('track', url1);
       // required when protocol is not "file:"
-      url.searchParams.set('ref', '');
+      link.searchParams.set('ref', '');
       // these are not required, but they look nicer
-      url.searchParams.set('artwork', 'small');
-      url.searchParams.set('size', 'large');
-      e_i.src = 'https://f4.bcbits.com/img/' + attr.shift() + '_16.jpg';
+      link.searchParams.set('artwork', 'small');
+      link.searchParams.set('size', 'large');
+      e_i.src = 'https://f4.bcbits.com/img/' + url2 + '_16.jpg';
       break;
    case 'g':
       // we need the trailing slash to maintain HTTPS
-      url = new URL('/umber/listen/', location);
-      url.searchParams.set('v', vdeo[0]);
+      link = new URL('/umber/listen/', location);
+      link.searchParams.set('v', tstamp);
 
-      e_i.src = 'https://github.com/cup/umber/releases/download/' +
-      attr.shift() + '/image.jpg';
+      e_i.src = 'https://github.com/cup/umber/releases/download/' + url1 +
+      '/image.jpg';
 
       break;
    case 's':
-      url = new URL('https://w.soundcloud.com/player');
-      url.hash = slug(vdeo[3]);
-      url.searchParams.set('url', 'api.soundcloud.com/tracks/' + attr.shift());
+      link = new URL('https://w.soundcloud.com/player');
+      link.hash = slug(title);
+      link.searchParams.set('url', 'api.soundcloud.com/tracks/' + url1);
       // ignored on mobile
-      url.searchParams.set('auto_play', true);
+      link.searchParams.set('auto_play', true);
       // accepts "true" but not "1"
-      url.searchParams.set('hide_related', true);
+      link.searchParams.set('hide_related', true);
       // these are not required, but it looks nicer
-      url.searchParams.set('show_comments', false);
-      url.searchParams.set('visual', true);
-
-      e_i.src = 'https://i1.sndcdn.com/artworks-' + attr.shift() +
-      '-t500x500.jpg';
-
+      link.searchParams.set('show_comments', false);
+      link.searchParams.set('visual', true);
+      e_i.src = 'https://i1.sndcdn.com/artworks-' + url2 + '-t500x500.jpg';
       break;
    case 'v':
       // player.vimeo.com/video/101914072: this video cannot be played here
-      url = new URL('https://vimeo.com/' + attr.shift());
-      url.hash = slug(vdeo[3]);
-      url.searchParams.set('autoplay', 1);
-
-      e_i.src = 'https://i.vimeocdn.com/video/' + attr.shift() +
-      '_1280x720.jpg';
-
+      link = new URL('https://vimeo.com/' + url1);
+      link.hash = slug(title);
+      link.searchParams.set('autoplay', 1);
+      e_i.src = 'https://i.vimeocdn.com/video/' + url2 + '_1280x720.jpg';
       break;
    case 'y':
       // video unavailable: youtube.com/embed/4Dcoz65iKQM
-      url = new URL('https://www.youtube.com/watch');
-      url.searchParams.set('v', attr.shift());
-      url.hash = slug(vdeo[3]);
+      link = new URL('https://www.youtube.com/watch');
+      link.searchParams.set('v', url1);
+      link.hash = slug(title);
 
-      e_i.src = 'https://i.ytimg.com/vi/' + url.searchParams.get('v') +
+      e_i.src = 'https://i.ytimg.com/vi/' + link.searchParams.get('v') +
       '/sd1.jpg';
    }
 
-   e_a.href = url.href;
-   e_d2.textContent = vdeo[3];
+   e_a.href = link.href;
+   e_d2.textContent = title;
 
-   e_fc.textContent = 'released ' + vdeo[1] +
-   ' - posted ' + new Date(vdeo[0] * 1000).toDateString();
+   e_fc.textContent = 'released ' + year +
+   ' - posted ' + new Date(tstamp * 1000).toDateString();
 
    e_d1.append(e_i);
    e_a.append(e_d1, e_d2);
@@ -110,7 +121,7 @@ else {
 fetch('/umber/radio/assets/data.json').then(aa => aa.json()).then(bb => {
    // both sides of the test can contain uppercase on mobile
    const result = bb.filter(cc => RegExp(query, 'i').test(cc[1] + cc[3]));
-   gei('figures').append(...result.slice(begin, end).map(dd => fgr(dd)));
+   gei('figures').append(...result.slice(begin, end).map(dd => fgr(...dd)));
 
    if (result[end]) {
       spar.set('p', page + 1);
