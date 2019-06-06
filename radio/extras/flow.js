2dcc2b85d6575a8ee532a5fc89b57c186c4ca7f6
Steven Penny <svnpenn@gmail.com>
2019-06-06 06:43:35 -0500

    {

--- radio/extras/flow.js
+++ radio/extras/flow.js
@@ -1,81 +1,83 @@
 'use strict';
-var qo = ae => document.querySelector(ae);
-var qa = ae => document.querySelectorAll(ae);
+{
+   const qo = ae => document.querySelector(ae);
+   const qa = ae => document.querySelectorAll(ae);
 
-/* we need to base this number on the watch page and not the result page for
-accuracy. this will give us some false highs but that is better than false
-lows. */
-var thold = 6000000;
+   /* we need to base this number on the watch page and not the result page
+   for accuracy. this will give us some false highs but that is better than
+   false lows. */
+   const thold = 6000000;
 
-/* get publish date */
-var dp = qo('[itemprop=datePublished]');
+   /* get publish date */
+   const dp = qo('[itemprop=datePublished]');
 
-if (dp) {
-   /* get views */
-   var vw = qo('[itemprop=interactionCount]').content;
-
-   /* get years */
-   var yr = (Date.now() - Date.parse(dp.content)) / Date.parse(1971);
-
-   var xc = document.createElement('textarea');
-   xc.cols = 50;
-   xc.rows = 4;
-   xc.style.font = 'medium Consolas';
-   xc.style.position = 'fixed';
-   xc.style.right = 0;
-   xc.style.top = '50px';
-   xc.style.zIndex = 6;
-
-   /* needs to work as bookmarklet */
-   xc.textContent = '   =:\n' +
-   '      ' + qo('[itemprop=name]').content
-   .replace(/^.* - /, '')
-   .replace(/ [([].*[)\]]$/, '')
-   .replace(/ A /, ' a ')
-   .replace(/'/, '’') + ':\n' +
-   '         flow: ' + Math.floor(vw / yr).toLocaleString() + '\n' +
-   '         link: ' + qo('[rel=shortlink]').href;
-
-   document.body.append(xc);
-}
-else {
-   qa('.yt-lockup-meta-info').forEach(ul => {
-      if (!ul.children[1]) {
-         return;
-      }
-
-      /* 1 year ago, Streamed 1 year ago */
-      var [t_n, t_w] = ul.children[0].textContent.split(' ').slice(-3);
-
+   if (dp) {
       /* get views */
-      var views = ul.children[1].textContent.split(' ')[0].replace(/,/g, '');
+      const vw = qo('[itemprop=interactionCount]').content;
 
-      switch (t_w) {
-      case 'year':
-      case 'years':
-         var fw = views / t_n;
-         break;
-      case 'month':
-      case 'months':
-         var fw = views * 12 / t_n;
-         break;
-      case 'week':
-      case 'weeks':
-         var fw = views * 52 / t_n;
-         break;
-      case 'day':
-      case 'days':
-         var fw = views * 365.25 / t_n;
-         break;
-      case 'hour':
-      case 'hours':
-         var fw = views * 24 * 365.25 / t_n;
-         break;
-      case 'minute':
-      case 'minutes':
-         var fw = views * 60 * 24 * 365.25 / t_n;
-      }
+      /* get years */
+      const yr = (Date.now() - Date.parse(dp.content)) / Date.parse(1971);
 
-      ul.style.border = fw < thold ? 'thick solid green' : 'thick solid red';
-   });
+      const xc = document.createElement('textarea');
+      xc.cols = 50;
+      xc.rows = 4;
+      xc.style.font = 'medium Consolas';
+      xc.style.position = 'fixed';
+      xc.style.right = 0;
+      xc.style.top = '50px';
+      xc.style.zIndex = 6;
+
+      /* needs to work as bookmarklet */
+      xc.textContent = '   =:\n' +
+      '      ' + qo('[itemprop=name]').content
+      .replace(/^.* - /, '')
+      .replace(/ [([].*[)\]]$/, '')
+      .replace(/ A /, ' a ')
+      .replace(/'/, '’') + ':\n' +
+      '         flow: ' + Math.floor(vw / yr).toLocaleString() + '\n' +
+      '         link: ' + qo('[rel=shortlink]').href;
+
+      document.body.append(xc);
+   }
+   else {
+      qa('.yt-lockup-meta-info').forEach(ul => {
+         if (!ul.children[1]) {
+            return;
+         }
+
+         /* 1 year ago, Streamed 1 year ago */
+         const [t_n, t_w] = ul.children[0].textContent.split(' ').slice(-3);
+
+         /* get views */
+         const views = ul.children[1].textContent.split(' ')[0].replace(/,/g, '');
+
+         switch (t_w) {
+         case 'year':
+         case 'years':
+            var fw = views / t_n;
+            break;
+         case 'month':
+         case 'months':
+            var fw = views * 12 / t_n;
+            break;
+         case 'week':
+         case 'weeks':
+            var fw = views * 52 / t_n;
+            break;
+         case 'day':
+         case 'days':
+            var fw = views * 365.25 / t_n;
+            break;
+         case 'hour':
+         case 'hours':
+            var fw = views * 24 * 365.25 / t_n;
+            break;
+         case 'minute':
+         case 'minutes':
+            var fw = views * 60 * 24 * 365.25 / t_n;
+         }
+
+         ul.style.border = fw < thold ? 'thick solid green' : 'thick solid red';
+      });
+   }
 }
